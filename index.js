2355ec5bc90c54834c332b4f116f80916bbccf9e
daliaalaa <daliaelhefnii@gmail.com>
2020-04-18 03:36:56 +0200

    Adding some packages

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const usersRoutes = require('./routes/users');
+const ordersRoutes = require('./routes/orders');
+const productsRoutes = require('./routes/products');
+
+
 
 
 const app = express();
 app.use(express.json());
 
- mongoose.connect('mongodb+srv://angularProject:angularProject@angularproject-p4l3j.mongodb.net/SouqDB?retryWrites=true&w=majority',
-// mongoose.connect('mongodb://localhost:27017/souqappdb',
-{
-useNewUrlParser:true,
-useUnifiedTopology:true
-}
-).then(()=>console.log('connected to MongoDb..'))
-.catch(err => console.log('failed to connect to Mongodb ',err.message));
+//  mongoose.connect('mongodb+srv://angularProject:angularProject@angularproject-p4l3j.mongodb.net/SouqDB?retryWrites=true&w=majority',
+mongoose.connect('mongodb://localhost:27017/souqappdb',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+).then(() => console.log('connected to MongoDb..'))
+    .catch(err => console.log('failed to connect to Mongodb ', err.message));
+
+app.use('/uploads', express.static('uploads'));
+
+app.use('/users', usersRoutes);
+app.use('/orders', ordersRoutes);
+
+app.use('/products', productsRoutes)
 
 
-app.use('/user',usersRoutes)
 
-app.listen(3000, () =>console.log("Server started at port 3000.."));
+app.listen(3000, () => console.log("Server started at port 3000.."));
